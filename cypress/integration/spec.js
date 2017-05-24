7744a029163579f909c8ee6342201a055565a208
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-05-24 11:09:48 -0400

    testing against HOST

---
+++ cypress/integration/spec.js
@@ -0,0 +1,10 @@
+describe('page', () => {
+  beforeEach(() => {
+    debugger
+    cy.visit(Cypress.env('HOST') || 'index.html')
+  })
+
+  it('has h2', () => {
+    cy.contains('h2', 'test')
+  })
+})
